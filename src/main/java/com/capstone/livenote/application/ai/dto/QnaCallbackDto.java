5559af088b3986485fbad7d0e871cad957db9c25
yoonsijin <trsj1017@naver.com>
2025-11-28 15:05:45 +0900

    [Fix] 요약 로직 변경

--- src/main/java/com/capstone/livenote/application/ai/dto/QnaCallbackDto.java
+++ src/main/java/com/capstone/livenote/application/ai/dto/QnaCallbackDto.java
@@ -1,5 +1,7 @@
 package com.capstone.livenote.application.ai.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
@@ -8,11 +10,21 @@ import java.util.List;
  * AI 서버가 생성한 QnA 콜백 DTO
  */
 @Getter @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class QnaCallbackDto {
+
+    @JsonProperty("lecture_id")
     private Long lectureId;
+
+    @JsonProperty("summary_id")
     private Long summaryId;
+
+    @JsonProperty("section_index")
     private Integer sectionIndex;
+
+    @JsonProperty("items")
     private List<QnaItem> qnaList;
+
 
     @Getter @Setter
     public static class QnaItem {
